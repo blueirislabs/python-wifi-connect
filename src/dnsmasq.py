3a8418794550e01ef4019305d1daf064e88237fc
Neal Jackson <nealsjackson@gmail.com>
2024-08-01 14:15:51 -0600

    use more reliable method for getting dnsmasq pid

--- src/dnsmasq.py
+++ src/dnsmasq.py
@@ -1,20 +1,24 @@
 # start / stop the dnsmasq process
 
 import subprocess, time
+import psutil
 
 DEFAULT_GATEWAY="192.168.42.1"
 DEFAULT_DHCP_RANGE="192.168.42.2,192.168.42.254"
 DEFAULT_INTERFACE="wlan0" # use 'ip link show' to see list of interfaces
 
+def get_dnsmasq_pid():
+    # Iterate over all running processes
+    for proc in psutil.process_iter(attrs=['pid', 'name']):
+        # Check if process name is 'dnsmasq'
+        if proc.info['name'] == 'dnsmasq':
+            return proc.info['pid']
+    return None
 
 def stop():
-    ps = subprocess.Popen("ps -e | grep ' dnsmasq' | cut -c 1-6", shell=True, stdout=subprocess.PIPE)
-    pid = ps.stdout.read()
-    ps.stdout.close()
-    ps.wait()
-    pid = pid.decode('utf-8')
-    pid = pid.strip()
-    if 0 < len(pid):
+    pid = get_dnsmasq_pid()
+    if pid is not None:
+        pid = str(pid)
         print(f"Killing dnsmasq, PID='{pid}'")
         ps = subprocess.Popen(f"kill -9 {pid}", shell=True)
         ps.wait()
